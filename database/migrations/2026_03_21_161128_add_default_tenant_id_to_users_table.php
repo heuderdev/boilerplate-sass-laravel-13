5ae7a4711627e9d562b1d8c7153cb9f8a97bf433
heuderdev <heuderdev@gmail.com>
2026-03-21 13:12:27 -0300

    [main]: feat: adicionando campo para escolher qual é o tenant padrão

---
+++ database/migrations/2026_03_21_161128_add_default_tenant_id_to_users_table.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->foreignId('default_tenant_id')->nullable()->constrained('tenants')->nullOnDelete()->after('email');
+        });
+    }
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropForeign(['default_tenant_id']);
+            $table->dropColumn('default_tenant_id');
+        });
+    }
+};
