82abc82bc76cd297e1959ee05f800918e3bbf992
heuderdev <heuderdev@gmail.com>
2026-03-21 21:17:32 -0300

    [view]: feat: add TenantForm, TenantTable, and TenantIndexPage components with corresponding Blade views

---
+++ app/Livewire/Tenant/Components/TenantTable.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Tenant\Components;
+
+use Livewire\Component;
+
+class TenantTable extends Component
+{
+    public function render()
+    {
+        return view('livewire.tenant.components.tenant-table');
+    }
+}
---
+++ app/Livewire/Tenant/Pages/TenantIndexPage.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Tenant\Pages;
+
+use Livewire\Component;
+
+class TenantIndexPage extends Component
+{
+    public function render()
+    {
+        return view('livewire.tenant.pages.tenant-index-page');
+    }
+}
